fc441d6e486a99975440cb494fc5621ebfcfe392
VirtUal <virtualprotoss@yahoo.com>
2016-07-08 09:09:05 +0300

    Add test property link calc

--- addons/resources/models/property_values.py
+++ addons/resources/models/property_values.py
@@ -7,10 +7,36 @@ class PropertyValues(models.Model):
 
     resource_id = fields.Many2one('resources.resources', 'Resource', select=True)
     property_id = fields.Many2one('resources.properties', 'Property', select=True)
-    value = fields.Char()
-    valueNum = fields.Integer()
-    valueType = fields.Selection([
+    _linked_value_id = fields.Many2one('resources.property_values', 'Linked value')
+    value = fields.Char(compute='_get_value')
+    real_value = fields.Char()
+    value_num = fields.Integer()
+    value_type = fields.Selection([
         ('V', 'Value'),
         ('DV', 'Depend Value'),
         ('MDV', 'Multi Depend Value'),
     ])
+
+    @api.depends('real_value', 'value_type', '_linked_value_id.value')
+    def _get_value(self):
+        if self.value_type == 'V':
+            self.value = self.real_value
+        elif self.value_type == 'DV':
+            self.value = self._linked_value_id.value
+        elif self.value_type == 'MDV':
+            # TODO: process the value_num
+            self.value = self._linked_value_id.value
+        else:
+            self.value = None
+
+    @api.onchange('value')
+    def _onchange_value(self):
+        if self.value_type == 'V':
+            self.real_value = self.value
+        elif self.value_type == 'DV':
+            self.real_value = self.search([('value_type', '=', 'V'), ('real_value', '=', self.value)], limit=1).id
+        elif self.value_type == 'MDV':
+            # TODO: process the value_num
+            self.real_value = self.search([('value_type', '=', 'V'), ('real_value', '=', self.value)], limit=1).id
+        else:
+            self.value = None
