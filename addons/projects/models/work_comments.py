1bbe095c43674bb1414b050c6a0e0b626f734804
VirtUal <virtualprotoss@yahoo.com>
2016-07-19 10:49:15 +0300

    Add button and form for add comments to work

--- addons/projects/models/work_comments.py
+++ addons/projects/models/work_comments.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
+from datetime import datetime
 
 
 class WorkComments(models.Model):
@@ -10,3 +11,21 @@ class WorkComments(models.Model):
     employee_id = fields.Many2one('hr.employee', 'Employee', select=True)
     date_comment = fields.Datetime()
     text = fields.Text()
+
+
+    @api.one
+    @api.depends('work_id', 'employee_id', 'date_comment', 'text')
+    def add_comment(self, rec):
+        if rec['active_model'] == 'projects.works':
+            work_id = rec['active_id']
+        else:
+            work_id = None
+        self.work_id = work_id
+        self.date_comment = datetime.now()
+        # self.text already stored before call function
+        resource = self.env['resource.resource'].search([('user_id', '=', rec['uid'])])
+        employee = self.env['hr.employee'].search([('resource_id', '=', resource.ids[0])])
+        if employee.id:
+            self.employee_id = employee.id
+
+        return {'type': 'ir.actions.act_window_close'}
