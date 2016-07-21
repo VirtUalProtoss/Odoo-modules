712b5cf91cc6850d6e503d58a277c6d60eefe668
VirtUal <virtualprotoss@yahoo.com>
2016-07-21 11:16:39 +0300

    Add oracle_fdw, add external_source_db module to odoo

---
+++ addons/onyma/models/apidogattrib.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+
+class OnymaServers(models.Model):
+    _name = 'onyma.api_dog_attrib'
+    _table = '_Linked_Table_Name_'
+
+
+    def create(self):
+        pass
+
+    def write(self):
+        pass
+
+    def unlink(self):
+        pass
